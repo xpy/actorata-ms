500988f31ba4ea24114d83cb98e769b7fc8069ec
xpy <pavloschris@gmail.com>
2018-01-14 23:18:40 +0100

    add psycopg2 in requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
 setup(name='actorata-ms',
-      install_requires=['Flask>=0.10.1','flask-cors>=3.0.3'],
+      install_requires=['Flask>=0.10.1',
+                        'flask-cors>=3.0.3',
+                        'psycopg2>=2.7'],
       )
